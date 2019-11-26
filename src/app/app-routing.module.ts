6f6b3bbef6191f0eb31af795ad1624b966cf90db
Suraj-B-Chettri <suraj.bohara.589@gmail.com>
2019-11-27 00:01:41 +0530

    day 2 component created and added to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Day2Component } from './day2/day2.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/day-two',
+    component: Day2Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Day2Component } from './day2/day2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Day2Component
   ],
   imports: [
     BrowserModule,
